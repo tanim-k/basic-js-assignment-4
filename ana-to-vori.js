1bd49394e482e639e2004289c83c7e5eb75e622d
tanim-k <tanimkhan877@gmail.com>
2022-02-05 09:30:03 +0600

    ana to vori and total cost

---
+++ ana-to-vori.js
@@ -0,0 +1,26 @@
+/* Assignment problem- 1 */
+function anaToVori(ana) {
+    const vori = ana / 16;
+    return vori;
+}
+    //🎀 function calling //🎀
+const herAna = anaToVori(16*2*3);
+console.log(herAna);
+/* Assignment problem- 2 */
+function pandaCost(singaraQuantity, samusaQuantity, jalebiQuantity) {
+    const perSingaraCost = 7;
+    const perSamusaCost = 10;
+    const perJalebiCost = 15;
+    /* finding total cost */
+    const singaraQuantityCost = perSingaraCost * singaraQuantity;
+    const samusQuantityCost = perSamusaCost * samusaQuantity;
+    const jalebiQuantityCost = perJalebiCost * jalebiQuantity;
+    const totalCost = singaraQuantityCost + samusQuantityCost + jalebiQuantityCost;
+    return totalCost;
+}
+    //🎀 function calling //🎀
+let babasTotalCost = pandaCost(7, 10, 15);
+console.log(babasTotalCost);
+/* Assignment problem- 3 */
+/* Assignment problem- 4 */
+/* Assignment problem- 5 */
